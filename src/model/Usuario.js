8233c8a708fa134b2980384acda822690ca7dc14
Roney V. Silva <roneyvsilva@gmail.com>
2023-03-21 20:47:47 -0300

    Feat: Usuario model

---
+++ src/model/Usuario.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const UsuarioSchema = new mongoose.Schema({
+    nome: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    senha: { type: String, required: true },
+    imagem: { type: String, required: true },
+    enderecos: [{
+        rua: { type: String, required: true },
+        numero: { type: Number, required: true },
+        complemento: { type: String, required: false },
+        cep: { type: String, required: true },
+        createAt: { type: Date, required: true }
+    }],
+    createAt: { type: Date, required: true },
+    produtosFavoritos: [{
+        _id: { type: mongoose.Schema.Types.ObjectId, required: true, unique: true, ref: "produtos" },
+        createAt: { type: Date, required: true }
+    }],
+    admin: { type: Boolean, required: true, default: false }
+})
+
+const Usuario = mongoose.model("usuarios", UsuarioSchema);
+
+module.exports = Usuario;
