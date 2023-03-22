c08cebb6e52866c3a089cd83c370d5f02e50ab5f
Roney V. Silva <roneyvsilva@gmail.com>
2023-03-21 21:16:01 -0300

    Feat: scripts, rotas de usuario e controller

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const connectDB = require("./src/database/database");
 const app = express();
 const port = 3000;
+const usuarioRouter = require("./src/router/usuario.router");
 
 app.use(express.json());
 
 connectDB();
 
+app.use("/usuario", usuarioRouter);
 app.get("/", (req, res) => {
     res.send({ message: "Bem vindo ao Market Place..." });
 })
