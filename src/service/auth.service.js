fe2fe42320fb31a7a91f5ce64524a43d35dd9cc8
Roney V. Silva <roneyvsilva@gmail.com>
2023-03-23 09:11:30 -0300

    Fix: instalação do dotenv e implementação

--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -1,13 +1,11 @@
 const Usuario = require("../model/Usuario");
 const jwt = require("jsonwebtoken");
 
-const segredo = "ed727127-8f55-420b-b992-c9792b546475";
 const loginService = (email) => Usuario.findOne({ email: email }).select("senha");
-const generateToken = (userId) => jwt.sign({ id: userId }, segredo, { expiresIn: "1d" });
+const generateToken = (userId) => jwt.sign({ id: userId }, process.env.SECRET, { expiresIn: "1d" });
 
 
 module.exports = {
     loginService,
-    generateToken,
-    segredo
+    generateToken
 }
